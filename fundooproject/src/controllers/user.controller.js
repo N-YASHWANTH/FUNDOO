6e36baec1ed8678ac2fcf48422e841268856c0e0
N Yashwanth <yashsai8629@gmail.com>
2023-12-21 12:33:24 +0530

    Created User UserRegistration API

---
+++ fundooproject/src/controllers/user.controller.js
@@ -0,0 +1,17 @@
+import * as userService from '../services/user.service'
+import HttpStatus from 'http-status-codes'
+
+export const newUser = async(req,res)=>{
+  try {
+    const data = await userService.registerUser(req.body)
+    res.status(HttpStatus.CREATED).json({
+      code: HttpStatus.CREATED,
+      data: data,
+      message: 'User Created Successfully'
+    })
+  } catch (error) {
+    res.status(HttpStatus.BAD_REQUEST).json({
+      message: error.message
+    })
+  }
+}
---
+++ fundooproject/src/services/user.service.js
@@ -0,0 +1,14 @@
+import User from '../models/user.model'
+
+export const registerUser = async(body)=>{
+  const data=await User.findOne({
+    email:body.email
+  })
+  if(data!=null){
+    throw new Error ('User already exists');
+  }
+  else{
+    const data=await User.create(body)
+    return data
+  }
+}
