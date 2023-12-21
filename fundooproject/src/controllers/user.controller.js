b62a6edd415467b6c0362d9d9f1ab979225f85ca
N Yashwanth <yashsai8629@gmail.com>
2023-12-21 13:50:25 +0530

    Created Login Api using bcrypt

--- fundooproject/src/controllers/user.controller.js
+++ fundooproject/src/controllers/user.controller.js
@@ -1,13 +1,13 @@
 import * as userService from '../services/user.service'
 import HttpStatus from 'http-status-codes'
 
-export const newUser = async(req,res)=>{
+export const loginUser = async(req,res)=>{
   try {
-    const data = await userService.registerUser(req.body)
-    res.status(HttpStatus.CREATED).json({
-      code: HttpStatus.CREATED,
+    const data = await userService.loginUser(req.body)
+    res.status(HttpStatus.ACCEPTED).json({
+      code: HttpStatus.ACCEPTED,
       data: data,
-      message: 'User Created Successfully'
+      message: 'User logged in Successfully'
     })
   } catch (error) {
     res.status(HttpStatus.BAD_REQUEST).json({
--- fundooproject/src/services/user.service.js
+++ fundooproject/src/services/user.service.js
@@ -1,19 +1,17 @@
 import User from '../models/user.model'
 import bcrypt from 'bcrypt'
 
-export const registerUser = async(body)=>{
+export const loginUser = async(body)=>{
   const data=await User.findOne({
     email:body.email
   })
-  if(data!=null){
-    throw new Error ('User already exists');
+  if(!data){
+    throw new Error ('User doesnot exists');
+  }
+  else if(!bcrypt.compare(data.password,body.password)){
+    throw new Error('Invalid password')
   }
   else{
-    const hashedPassword=await bcrypt.hash(body.password,10)
-    const data=await User.create({
-      ...body,
-      password: hashedPassword
-    })
     return data
   }
 }
