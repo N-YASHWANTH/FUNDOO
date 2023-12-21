c1b81865cc43298dc5de6f75f3c23f28699c284b
N Yashwanth <yashsai8629@gmail.com>
2023-12-21 12:46:11 +0530

    Created Login Api

--- fundooproject/src/services/user.service.js
+++ fundooproject/src/services/user.service.js
@@ -1,15 +1,16 @@
 import User from '../models/user.model'
 
-export const registerUser = async(body)=>{
+export const loginUser = async(body)=>{
   const data=await User.findOne({
     email:body.email
   })
-  if(data!=null){
-    throw new Error ('User already exists');
+  if(!data){
+    throw new Error ('User not found');
   }
-   
+  else if(data.password!=body.password){
+    throw new Error ('Invalid User')
+  }
   else{
-    const data=await User.create(body)
     return data
   }
 }
